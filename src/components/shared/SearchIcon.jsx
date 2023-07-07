266856fc9fa43d8f201a8654c4c99e4f01f4a1ff
nazar-4real <marusyk.nazar@gmail.com>
2023-07-07 13:49:14 +0300

    color of the search icon changes depending on the theme

--- src/components/shared/SearchIcon.jsx
+++ src/components/shared/SearchIcon.jsx
@@ -3,12 +3,19 @@ import { useContext } from 'react'
 import { ThemeContext } from 'src/context/ThemeStore'
 
 export const SearchIcon = () => {
-  const { theme: { value, colors: { action, text } } } = useContext(ThemeContext)
+  const { theme: { colors: { action } } } = useContext(ThemeContext)
 
   return (
-    <svg className="search-icon" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke={value === 'plum' ? action : text} strokeOpacity="0.38" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M21 20.9999L16.65 16.6499" stroke={value === 'plum' ? action : text} strokeOpacity="0.38" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <svg
+      className="search-icon"
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ color: action }}>
+      <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="currentColor" strokeOpacity="0.38" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M21 20.9999L16.65 16.6499" stroke="currentColor" strokeOpacity="0.38" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 
   )
