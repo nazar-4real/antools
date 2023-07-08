819d8dc4a37b11064cdaf685326ed9521656a0f0
nazar-4real <marusyk.nazar@gmail.com>
2023-07-09 01:26:54 +0300

    changed stylization of tool page

--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -2,14 +2,15 @@ import { useContext } from 'react'
 
 import { ThemeContext } from 'src/context/ThemeStore'
 
-export const Title = ({ children, ...props }) => {
+export const Title = ({ children, style, ...props }) => {
   const { theme: { value, colors: { action } } } = useContext(ThemeContext)
   return (
     <h2
       className="main-title"
       style={{
         color: value !== 'default' && action,
-        ...(value === 'light' && { filter: 'brightness(.9)' })
+        ...(value === 'light' && { filter: 'brightness(.9)' }),
+        ...style
       }}
       {...props}>
       {children}
--- src/pages/ToolPage.jsx
+++ src/pages/ToolPage.jsx
@@ -36,17 +36,27 @@ const ToolPage = () => {
 
   return (
     <Section className="tool" style={styles}>
-      <div className="tool__product" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
-        <img src={icon} alt={name} style={{ width: '100px', height: '80px', objectFit: 'contain' }} />
-        <Title style={{ margin: 0 }}>{name}</Title>
-        <Text>{status}</Text>
-        <Text>{text}</Text>
-        <div className="tool__product-actions" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '20px' }}>
-          <LikeIcon style={{ ...(liked && { fill: '#dc143c', fillOpacity: 1 }) }} />
-          <AttachIcon style={{ ...(attached && { fill: '#0091ff', fillOpacity: 1 }) }} />
+      <div
+        className="tool__product"
+        style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '50px' }}>
+        <div className="tool__product-img" style={{ flex: '0 1 200px' }}>
+          <figure className="tool__figure">
+            <img src={icon} alt={name} style={{ width: '100px', height: '80px', objectFit: 'contain' }} />
+          </figure>
+          <div className="tool__product-actions" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '40px', marginTop: '50px' }}>
+            <LikeIcon style={{ ...(liked && { fill: '#dc143c', fillOpacity: 1 }) }} />
+            <AttachIcon style={{ ...(attached && { fill: '#0091ff', fillOpacity: 1 }) }} />
+          </div>
+        </div>
+        <div className="tool__product-info" style={{ textAlign: 'left' }}>
+          <div className="tool__product-caption" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '15px' }}>
+            <Title style={{ margin: 0 }}>{name}</Title>
+            <Text>{status}</Text>
+          </div>
+          <Text>{text}</Text>
+          <Button onClick={() => navigate(-1)} style={{ marginTop: '40px' }}>Go back</Button>
         </div>
       </div>
-      <Button onClick={() => navigate(-1)} style={{ marginTop: '40px' }}>Go back</Button>
     </Section>
   )
 }
