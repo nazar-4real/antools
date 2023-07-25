79ee7eafef18a6c04aaca006a1f1f36eff2e8f93
nazar-4real <marusyk.nazar@gmail.com>
2023-07-25 17:26:31 +0300

    moving data to separate files

--- src/components/newcomer/Newcomer.jsx
+++ src/components/newcomer/Newcomer.jsx
@@ -9,56 +9,12 @@ import { Button } from '../shared/Button'
 
 import { ToolCard } from '../shared/ToolCard'
 
-import zeplin from '../../assets/images/newcomer/zeplin.svg'
-import phpstorm from '../../assets/images/newcomer/phpstorm.svg'
-import toolbox from '../../assets/images/newcomer/toolbox.svg'
-import procreate from '../../assets/images/newcomer/procreate.svg'
-
 import { onPropToggle } from 'src/pages/Homepage'
 
+import { newcomerToolsArr } from 'src/data/newcomerData'
+
 const Newcomer = () => {
-  const [newcomerData, setNewcomerData] = useState([
-    {
-      id: 1,
-      icon: zeplin,
-      name: 'Zeplin',
-      status: 'Free & Paid',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      liked: false,
-      attached: false,
-      url: 'https://zeplin.io/'
-    },
-    {
-      id: 2,
-      icon: phpstorm,
-      name: 'PHPStorm',
-      status: 'Free',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      liked: false,
-      attached: false,
-      url: 'https://www.jetbrains.com/phpstorm/'
-    },
-    {
-      id: 3,
-      icon: toolbox,
-      name: 'Toolbox',
-      status: 'Free',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      liked: false,
-      attached: false,
-      url: 'https://www.jetbrains.com/toolbox-app/'
-    },
-    {
-      id: 4,
-      icon: procreate,
-      name: 'Procreate',
-      status: 'Paid',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      liked: false,
-      attached: false,
-      url: 'https://procreate.com/'
-    }
-  ])
+  const [newcomerData, setNewcomerData] = useState(newcomerToolsArr)
 
   const propHandler = (prop, id) => {
     onPropToggle(prop, id, setNewcomerData)
---
+++ src/data/newcomerData.js
@@ -0,0 +1,47 @@
+import zeplin from 'src/assets/images/newcomer/zeplin.svg'
+import phpstorm from 'src/assets/images/newcomer/phpstorm.svg'
+import toolbox from 'src/assets/images/newcomer/toolbox.svg'
+import procreate from 'src/assets/images/newcomer/procreate.svg'
+
+export const newcomerToolsArr = [
+  {
+    id: 1,
+    icon: zeplin,
+    name: 'Zeplin',
+    status: 'Free & Paid',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    liked: false,
+    attached: false,
+    url: 'https://zeplin.io/'
+  },
+  {
+    id: 2,
+    icon: phpstorm,
+    name: 'PHPStorm',
+    status: 'Free',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    liked: false,
+    attached: false,
+    url: 'https://www.jetbrains.com/phpstorm/'
+  },
+  {
+    id: 3,
+    icon: toolbox,
+    name: 'Toolbox',
+    status: 'Free',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    liked: false,
+    attached: false,
+    url: 'https://www.jetbrains.com/toolbox-app/'
+  },
+  {
+    id: 4,
+    icon: procreate,
+    name: 'Procreate',
+    status: 'Paid',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    liked: false,
+    attached: false,
+    url: 'https://procreate.com/'
+  }
+]
