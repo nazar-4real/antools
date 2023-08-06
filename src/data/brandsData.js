0a3013a296453c8940d952179c88e1cdbfb77dcb
nazar-4real <marusyk.nazar@gmail.com>
2023-08-07 01:03:46 +0300

    moved brands data to separate file

---
+++ src/data/brandsData.js
@@ -0,0 +1,27 @@
+import microsoft from 'src/assets/images/brands/microsoft.svg'
+import google from 'src/assets/images/brands/google.svg'
+import slack from 'src/assets/images/brands/slack.svg'
+import wordpress from 'src/assets/images/brands/wordpress.svg'
+
+export const brandsData = [
+  {
+    id: 1,
+    icon: microsoft,
+    name: 'Microsoft'
+  },
+  {
+    id: 2,
+    icon: google,
+    name: 'Google'
+  },
+  {
+    id: 3,
+    icon: slack,
+    name: 'Slack'
+  },
+  {
+    id: 4,
+    icon: wordpress,
+    name: 'WordPress'
+  },
+]
--- src/pages/HomePage/components/brands/Brands.jsx
+++ src/pages/HomePage/components/brands/Brands.jsx
@@ -7,33 +7,7 @@ import './brands.scss'
 import { Section } from 'src/components/shared/Section'
 import { Title } from 'src/components/shared/Title'
 
-import microsoft from 'src/assets/images/brands/microsoft.svg'
-import google from 'src/assets/images/brands/google.svg'
-import slack from 'src/assets/images/brands/slack.svg'
-import wordpress from 'src/assets/images/brands/wordpress.svg'
-
-const brandsData = [
-  {
-    id: 1,
-    icon: microsoft,
-    name: 'Microsoft'
-  },
-  {
-    id: 2,
-    icon: google,
-    name: 'Google'
-  },
-  {
-    id: 3,
-    icon: slack,
-    name: 'Slack'
-  },
-  {
-    id: 4,
-    icon: wordpress,
-    name: 'WordPress'
-  },
-]
+import { brandsData } from 'src/data/brandsData'
 
 const Brands = () => {
   const { theme: { value, colors: { action, text } } } = useContext(ThemeContext)
