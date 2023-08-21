8786cfa5f505bdb2b5b6f50c80551935d58bdcfc
nazar-4real <marusyk.nazar@gmail.com>
2023-08-21 12:49:22 +0300

    replaced fetch with axios

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,10 +1,12 @@
+import axios from 'axios'
+
 class DataService {
   _api = import.meta.env.VITE_APP_DATABASE_URL
 
   getResource = async (url) => {
     try {
-      let response = await fetch(url)
-      return await response.json()
+      const response = await axios(url)
+      return response.data
     } catch (error) {
       console.error(error)
       throw new Error(`Failed to get data from ${url}`)
